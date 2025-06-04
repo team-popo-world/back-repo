6f0f804973405037ece13c712b219e1baece714e
ooinl77 <ooinl77@naver.com>
2025-06-04 11:34:10 +0900

    Fix: 구조 변경

--- src/main/java/com/popoworld/backend/kafka/LogMessage.java
+++ src/main/java/com/popoworld/backend/kafka/LogMessage.java
@@ -1,6 +1,8 @@
 package com.popoworld.backend.kafka;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.annotation.Id;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Document(collection = "log_messages")
 @Data
@@ -15,7 +18,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class LogMessage {
     @Id
-    private String id;
+    private UUID id;
     private String userId;
     private String type;
     private String message;
