aba8237d2549496475925a25f81602f69ef0687d
ooinl77 <ooinl77@naver.com>
2025-06-04 17:41:57 +0900

    Delete: mongo 테스트 코드 삭제

--- src/main/java/com/popoworld/backend/kafka/LogMessage.java
+++ src/main/java/com/popoworld/backend/kafka/LogMessage.java
@@ -6,13 +6,11 @@ import jakarta.persistence.GenerationType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.annotation.Id;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Document(collection = "log_messages")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
